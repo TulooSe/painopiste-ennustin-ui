38f4f34824b2fb61a5988316ebc09e7a06309622
TulooSe <tulooko.valamista@gmail.com>
2026-02-25 19:17:21 +0200

    Create startView.js

---
+++ js/startView.js
@@ -0,0 +1,56 @@
+import { state, setView } from "./state.js";
+import { renderApp } from "./router.js";
+
+export function renderStart() {
+
+  const container = document.getElementById("startView");
+  container.style.display = "block";
+
+  container.innerHTML = `
+    <h2>Lennokit</h2>
+    <table>
+      <tbody>
+        ${state.lennokit.map(l => `
+          <tr data-id="${l.id}">
+            <td>${l.id}</td>
+            <td>${l.massa ?? ""}</td>
+            <td>${l.pp ?? ""}</td>
+            <td>${l.pvm ?? ""}</td>
+          </tr>
+        `).join("")}
+      </tbody>
+    </table>
+
+    <button id="openEditorBtn">Avaa</button>
+  `;
+
+  bindStartEvents();
+}
+
+
+/*Event delegation*/
+
+function bindStartEvents() {
+
+  const container = document.getElementById("startView");
+
+  container.addEventListener("click", e => {
+
+    const row = e.target.closest("tr");
+    if (row) {
+      state.valittuLennokkiId = row.dataset.id;
+
+      container.querySelectorAll("tr")
+        .forEach(r => r.classList.remove("selected"));
+
+      row.classList.add("selected");
+    }
+
+    if (e.target.id === "openEditorBtn") {
+      if (!state.valittuLennokkiId) return;
+
+      setView("editor");
+      renderApp();
+    }
+  });
+}
