1768f124a150dd75e6aac6be6b659a43a26074fa
TulooSe <tulooko.valamista@gmail.com>
2026-02-27 22:14:30 +0200

    Update app_combined.js

--- js/app_combined.js
+++ js/app_combined.js
@@ -423,3 +423,25 @@ function renderEditor() {
 
   bindEditorEvents();
 }
+
+
+
+function renderApp() {
+  hideAllViews();
+
+  switch (state.view) {
+    case "start":
+      renderStart();
+      break;
+
+    case "editor":
+      renderEditor();
+      break;
+  }
+}
+
+function hideAllViews() {
+  document.getElementById("startView").style.display = "none";
+  document.getElementById("editorView").style.display = "none";
+}
+
