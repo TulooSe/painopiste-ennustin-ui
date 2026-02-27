13ea6c7c45344d1fdcf73a2ca362ce5b3c1d4eb7
TulooSe <tulooko.valamista@gmail.com>
2026-02-27 22:13:59 +0200

    Update app_combined.js

--- js/app_combined.js
+++ js/app_combined.js
@@ -407,3 +407,19 @@ function bindStartEvents() {
     }
   });
 }
+
+
+
+function renderEditor() {
+
+  const container = document.getElementById("editorView");
+  container.style.display = "block";
+
+  container.innerHTML = `
+    <h2>Editor</h2>
+    <div id="osatView"></div>
+    <button id="backBtn">Takaisin</button>
+  `;
+
+  bindEditorEvents();
+}
