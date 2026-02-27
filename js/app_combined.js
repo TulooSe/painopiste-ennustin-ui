31ce7f7d475f40bad468cd2803ae00bd4a2d2b08
TulooSe <tulooko.valamista@gmail.com>
2026-02-27 22:11:24 +0200

    Update app_combined.js

--- js/app_combined.js
+++ js/app_combined.js
@@ -124,4 +124,34 @@ const Auth = (function () {
     logout
   };
 
+
+
+const state = {
+  view: "start",        // start | editor
+  lennokit: [],
+  valittuLennokkiId: null,
+
+  osat: [],
+  aktiivinenRyhma: null,
+  suodataNollat: false,
+  kokoonpanot: [],
+
+  uusiLennokki: {
+    nimi: "",
+    malli: "RAKENTEILLA"
+  }
+};
+
+function setView(view) {
+  state.view = view;
+}
+
+function setOsat(osat) {
+  state.osat = osat;
+}
+
+function setLennokit(lennokit) {
+  state.lennokit = lennokit;
+}
+  
 })();
